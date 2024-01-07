745c7bddb7ce49a0e80343207824c372627872f9
rohit <rohit@172.16.36.68>
2024-01-07 18:07:16 +0530

    Initial commit - doc change

--- nonrest/src/main/java/payroll/Employee.java
+++ nonrest/src/main/java/payroll/Employee.java
@@ -6,10 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-/**
- * Employee is the main entity used 
- * 
- */
+
 @Entity
 class Employee {
 
